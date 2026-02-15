dc2f3b3d54ed8a6fe3974aadf7707fbde167d60c
admin <admin@DESKTOP-5FF9V40>
2026-02-15 18:12:49 +0800

    Pharmacist Title Bar

--- electron/main.js
+++ electron/main.js
@@ -1,23 +1,61 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path') // <--- Add this at the top
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+
+// CHANGE 1: Declare 'win' here (outside the function)
+let win; 
 
 function createWindow () {
-  const win = new BrowserWindow({
+  // CHANGE 2: Remove 'const' or 'let' here. 
+  // We want to use the 'win' variable we created above.
+  win = new BrowserWindow({
     width: 1200,
     height: 800,
+    frame: false,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false // often needed for simple migrations
+      contextIsolation: false
     }
+    
   })
 
-  // This tells Electron: Go up one level (..), into frontend, into html, find index.html
-  // Make sure your main html file is actually named 'index.html'
-  win.loadFile(path.join(__dirname, '../frontend/html/index.html')) 
+  win.on('maximize', () => {
+    win.webContents.send('is-maximized');
+  });
+
+  win.on('unmaximize', () => {
+    win.webContents.send('is-restored');
+  });
+
+  win.loadFile(path.join(__dirname, '../frontend/html/index.html'))
 }
 
 app.whenReady().then(createWindow)
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
+// Now these will work because they can access 'win'
+ipcMain.on('minimize-app', () => {
+  if (win) win.minimize();
+});
+
+ipcMain.on('maximize-app', () => {
+  if (win) {
+    if (win.isMaximized()) {
+      win.unmaximize();
+    } else {
+      win.maximize();
+    }
+  }
+});
+
+ipcMain.on('close-app', () => {
+  if (win) win.close();
+});
+
+// main.js - Add this with your other ipcMain handlers
+ipcMain.on('check-maximized', (event) => {
+    if (win.isMaximized()) {
+        event.reply('is-maximized');
+    } else {
+        event.reply('is-restored');
+    }
+});
+
